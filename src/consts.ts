3988fdb833b7bf411765915bfd20308e6e26093c
do-n-khanh <do.n.khanh@gmail.com>
2026-01-08 13:42:45 +0900

    Add rental properties page with photo gallery, customer reviews, and updated content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "KindBridge";
+export const SITE_DESCRIPTION = "システム開発と不動産賃貸事業を展開する総合企業です。";
